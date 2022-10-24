26e5e6bd6641514ac76748c453550962eca936d0
drstrange20 <fatiha.94@mail.ru>
2022-10-24 07:41:47 +0300

    test5

--- src/ru/mysite/translator/Game.java
+++ src/ru/mysite/translator/Game.java
@@ -3,21 +3,21 @@ package ru.mysite.translator;
 import java.util.Scanner;
 
 class Game {
-    static final String [] ENGLISH_WORDS = {"redundant", "expression", "software", "application", "freeware", "compile", "compress", "hyperlink", "archive", "debug", "issue", "encrypt", "decrypt", "exception", "verify", "disable", "taught", "tough", "though", "thorough", "enable", "thought", "equal", "through", "utility", "throughout", "from scratch", "capability", "request", "gateway"};
-    static final String [] RUSSIAN_WORDS = {"излишний", "выражение", "програмное обеспечение", "приложение", "бесплатное ПО", "компилировать", "сжимать", "ссылка", "архивировать", "отлаживать", "проблема", "зашифровать", "расшифровывать", "исключение", "проверять", "запрещать", "учил", "жесткий", "хотя", "тщательный", "разрешать", "думал", "равный", "через", "утилита", "по всему", "с нуля", "возможности", "запрос", "шлюз"};
-    static Scanner scanInt = new Scanner(System.in);
-    static Scanner scanStr = new Scanner(System.in);
+    static final String[] ENGLISH_WORDS = {"redundant", "expression", "software", "application", "freeware", "compile", "compress", "hyperlink", "archive", "debug", "issue", "encrypt", "decrypt", "exception", "verify", "disable", "taught", "tough", "though", "thorough", "enable", "thought", "equal", "through", "utility", "throughout", "from scratch", "capability", "request", "gateway"};
+    static final String[] RUSSIAN_WORDS = {"излишний", "выражение", "програмное обеспечение", "приложение", "бесплатное ПО", "компилировать", "сжимать", "ссылка", "архивировать", "отлаживать", "проблема", "зашифровать", "расшифровывать", "исключение", "проверять", "запрещать", "учил", "жесткий", "хотя", "тщательный", "разрешать", "думал", "равный", "через", "утилита", "по всему", "с нуля", "возможности", "запрос", "шлюз"};
+    static Scanner scannerInteger = new Scanner(System.in);
+    static Scanner scannerString = new Scanner(System.in);
+    static int numberOfWords;
 
     public static void translateToEnglish(String name) {
         int russianLength = RUSSIAN_WORDS.length;
         int k = 1;
 
-        System.out.println("\nСколько слов Вы хотите перевести?\nВведите число от 1 до " +russianLength + ":");
-        int numberOfWords = howMatch(russianLength);
-
+        System.out.println("\nСколько слов Вы хотите перевести?\nВведите число от 1 до " + russianLength + ":");
+        int numberOfWords = chooseHowMuchWordsToTranslate(russianLength);
 
         for (int i = 0; i < russianLength; i++) {
-            int index = (int) (Math.random()*russianLength);
+            int index = (int) (Math.random() * russianLength);
 
             String rus = RUSSIAN_WORDS[i];
             RUSSIAN_WORDS[i] = RUSSIAN_WORDS[index];
@@ -28,13 +28,13 @@ class Game {
             ENGLISH_WORDS[index] = eng;
         }
 
-        for (int i = 0; i < numberOfWords; i++){
+        for (int i = 0; i < numberOfWords; i++) {
 
             System.out.println("\n" + k + ". " + RUSSIAN_WORDS[i]);
 
-            String translate = scanStr.nextLine();
+            String translate = scannerString.nextLine();
 
-            if (translate.equalsIgnoreCase(ENGLISH_WORDS[i])){
+            if (translate.equalsIgnoreCase(ENGLISH_WORDS[i])) {
                 System.out.println("Верно!");
             } else {
                 System.out.println("К сожалению, Вы ошиблись!\nПравильный ответ: " + ENGLISH_WORDS[i]);
@@ -48,12 +48,12 @@ class Game {
         int englishLength = ENGLISH_WORDS.length;
         int k = 1;
 
-        System.out.println("\nCколько слов Вы хотите перевести?\nВведите число от 1 до " +englishLength + ":");
-        int numberOfWords = howMatch(englishLength);
+        System.out.println("\nCколько слов Вы хотите перевести?\nВведите число от 1 до " + englishLength + ":");
+        int numberOfWords = chooseHowMuchWordsToTranslate(englishLength);
 
 
         for (int i = 0; i < englishLength; i++) {
-            int index = (int) (Math.random()*englishLength);
+            int index = (int) (Math.random() * englishLength);
 
             String eng = ENGLISH_WORDS[i];
             ENGLISH_WORDS[i] = ENGLISH_WORDS[index];
@@ -65,13 +65,13 @@ class Game {
 
         }
 
-        for (int i = 0; i < numberOfWords; i++){
+        for (int i = 0; i < numberOfWords; i++) {
 
             System.out.println("\n" + k + ". " + ENGLISH_WORDS[i]);
 
-            String translate = scanStr.nextLine();
+            String translate = scannerString.nextLine();
 
-            if (translate.equalsIgnoreCase(RUSSIAN_WORDS[i])){
+            if (translate.equalsIgnoreCase(RUSSIAN_WORDS[i])) {
                 System.out.println("Верно!");
             } else {
                 System.out.println("К сожалению, Вы ошиблись!\nПравильный ответ: " + RUSSIAN_WORDS[i]);
@@ -80,28 +80,29 @@ class Game {
         }
         playAgain(name);
     }
+
     static void playAgain(String name) {
         System.out.println("\nХотите сыграть еще раз?\nВведите \"да\" или \"нет\":");
 
-        String answer = scanStr.nextLine();
+        String answer = scannerString.nextLine();
         while (!answer.equalsIgnoreCase("да") && !answer.equalsIgnoreCase("нет")) {
             System.out.println("\nНекорректный ответ.");
-            answer = scanStr.nextLine();
+            answer = scannerString.nextLine();
         }
         if (answer.equalsIgnoreCase("нет")) {
-            System.out.println("\nСпасибо за игру, " + name +"!\nВозвращайтесь скорее! Будем тренировать Ваш Английский!");
+            System.out.println("\nСпасибо за игру, " + name + "!\nВозвращайтесь скорее! Будем тренировать Ваш Английский!");
         } else {
             chooseLanguage(name);
         }
     }
 
     static void chooseLanguage(String name) {
-        System.out.println("\n"+name + ", на какой язык хотите переводить?\nНапишите \"на русский\" или \"на английский\":");
+        System.out.println("\n" + name + ", на какой язык хотите переводить?\nНапишите \"на русский\" или \"на английский\":");
 
-        String answer = scanStr.nextLine();
+        String answer = scannerString.nextLine();
         while (!answer.equalsIgnoreCase("на русский") && !answer.equalsIgnoreCase("на английский")) {
             System.out.println("\nНекорректный ответ.");
-            answer = scanStr.nextLine();
+            answer = scannerString.nextLine();
         }
         if (answer.equalsIgnoreCase("на английский")) {
             translateToEnglish(name);
@@ -110,24 +111,30 @@ class Game {
         }
     }
 
-    static int howMatch(int englishLength) {
-        int numberOfWords = scanInt.nextInt();
-        while (numberOfWords < 1 || numberOfWords > englishLength) {
-
-            System.out.println("Введите число от 1 до " +englishLength + ":");
-
-            numberOfWords = scanInt.nextInt();
+    static int chooseHowMuchWordsToTranslate(int englishLength) {
+        while (true) {
+            try {
+                numberOfWords = Integer.parseInt(scannerInteger.nextLine().trim());
+                while (numberOfWords < 1 || numberOfWords > englishLength) {
+                    System.out.println("Введите число от 1 до " + englishLength + ":");
+                    numberOfWords = Integer.parseInt(scannerInteger.nextLine().trim());
+                }
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Введите число от 1 до " + englishLength + ":");
+            }
         }
         return numberOfWords;
     }
-    static String userName(){
-        String name = scanStr.nextLine();
+
+    static String verifyUserName() {
+        String name = scannerString.nextLine();
 
         while (name == null || name.trim().isEmpty()) {
 
             System.out.println("Вы ввели некорректное имя");
 
-            name = scanStr.nextLine();
+            name = scannerString.nextLine();
         }
         return name;
     }
--- src/ru/mysite/translator/Program.java
+++ src/ru/mysite/translator/Program.java
@@ -3,7 +3,7 @@ package ru.mysite.translator;
 public class Program {
     public static void main(String[] args) {
         System.out.println("Добро пожаловать в игру Переводчик!\nВведите Ваше имя:");
-        String name = Game.userName();
+        String name = Game.verifyUserName();
 
         Game.chooseLanguage(name);
     }
